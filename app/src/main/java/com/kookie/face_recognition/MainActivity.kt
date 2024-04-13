bc48289670b34799d1f2b58d5b3aa934a4241821
monsterKyu <ilkyu092@naver.com>
2024-04-13 15:00:54 +0900

    start setting

--- app/src/main/java/com/kookie/face_recognition/MainActivity.kt
+++ app/src/main/java/com/kookie/face_recognition/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.kookie.face_recognition.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
